03ce44d39cf0b448e33361b1956647d21ba5cded
Adarsh Yadav <adarsh.yadav070605@gmail.com>
2026-07-02 23:04:34 +0530

    chore: initialize MERN project structure

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# HireFlow AI
+
+An AI-powered Applicant Tracking System (ATS) built using the MERN Stack.
+
+## Tech Stack
+
+- React
+- Node.js
+- Express
+- MongoDB
+- Tailwind CSS
+- JWT Authentication
+- Cloudinary
+- Gemini API
+
+## Features
+
+- Candidate Dashboard
+- Recruiter Dashboard
+- Resume Upload
+- AI Resume Analysis
+- Job Management
+- Application Tracking
+
+> Project currently under development.
---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="min-h-screen bg-blue-600 flex items-center justify-center">
+      <h1 className="text-5xl text-white font-bold">
+        HireFlow AI
+      </h1>
+    </div>
+
+  )
+}
+
+export default App
